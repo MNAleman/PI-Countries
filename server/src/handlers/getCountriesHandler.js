9ab5e232bd5ca53300ace4539f4ecc1c4550164d
MNAleman <alemanmn@gmail.com>
2023-10-24 23:54:40 -0300

    getAllCountries

--- server/src/handlers/getCountriesHandler.js
+++ server/src/handlers/getCountriesHandler.js
@@ -3,7 +3,7 @@ const { getAllCountries } = require('../controllers/saveCountries');
 const getCountriesHandler = async (req, res) => {
     const { country } = req.query;
     try {
-        const response = await getAllCountries(country); // Proporciona un valor predeterminado si country no está definido
+        const response = await getAllCountries(country);
         res.status(200).json(response);
     } catch (error) {
         res.status(400).json({ error: error.message });
